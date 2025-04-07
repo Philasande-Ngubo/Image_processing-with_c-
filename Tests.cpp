948580b32de81b130b843b4e734adef3bc0fffca
Philasande Ngubo <ngbphi016@nightmare.cs.uct.ac.za>
2025-04-07 22:42:22 +0000

    Added a Test for the number of connected components

--- Tests.cpp
+++ Tests.cpp
@@ -7,6 +7,20 @@
 
 int one =1;
 int two =2;
+
+TEST_CASE ("Test number of  Connected Component")
+ {
+    PGMimage image;
+    image.read("Chess_Colours.pgm");
+    int wd,ht;
+    image.getDims(wd,ht);
+
+    PGMimageProcessor imageProcessor(wd,ht);
+    REQUIRE( imageProcessor.extractComponents( const_cast<unsigned char *>(image.getBuffer()),171 ) == 6);
+    
+ }
+
+
 TEST_CASE ("MinSize Connected Component")
  {
     PGMimage image;
@@ -34,3 +48,18 @@ TEST_CASE ("MinSize Connected Component")
     REQUIRE( imageProcessor.getLargestSize() == 7961);
     
  }
+
+ TEST_CASE (" Connected Components")
+ {
+    PGMimage image;
+    image.read("Chess_Colours.pgm");
+    int wd,ht;
+    image.getDims(wd,ht);
+
+    PGMimageProcessor imageProcessor(wd,ht);
+    imageProcessor.extractComponents( const_cast<unsigned char *>(image.getBuffer()),171 );
+
+    REQUIRE( imageProcessor.getLargestSize() == 7961);
+    
+ }
+
